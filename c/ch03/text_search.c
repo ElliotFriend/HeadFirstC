fc032b1ac83ca254f041d2700cc4dec93ba074bc
Elliot V <elliotfriend@gmail.com>
2013-04-02 00:50:36 -0500

    fully functional jukebox application (including errata fix) http://oreilly.com/catalog/errata.csp?isbn=0636920015482 -- search for page 94

--- c/ch03/text_search.c
+++ c/ch03/text_search.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <string.h>
 
 char tracks[][80] = {
     "I left my heart in Harvart Med School",
@@ -7,3 +8,22 @@ char tracks[][80] = {
     "From here to maternity",
     "The girl from Iwo Jima",
 };
+
+void find_track(char search_for[])
+{
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (strstr(tracks[i], search_for))
+            printf("Track %i: '%s'\n", i, tracks[i]);
+    }
+}
+
+int main()
+{
+    char search_for[80];
+    printf("Search for: ");
+    fgets(search_for, 80, stdin);
+    search_for[strlen(search_for) - 1] = '\0';
+    find_track(search_for);
+    return 0;
+}
