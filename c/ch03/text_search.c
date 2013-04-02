80009b825d08834c8ccf9bc32eb7f2c6912ba9dc
Elliot V <elliotfriend@gmail.com>
2013-04-01 23:45:08 -0500

    began "Desperately seeking Frank" section of chapter 3

---
+++ c/ch03/text_search.c
@@ -0,0 +1,9 @@
+
+
+char tracks[][80] = {
+    "I left my heart in Harvart Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima",
+};
