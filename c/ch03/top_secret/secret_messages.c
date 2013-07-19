ce763964a7b8b5af9dd1b4ecd3c0e5e708b40a73
Elliot Voris <elliotfriend@gmail.com>
2013-07-18 23:28:34 -0500

    fixing whitespace issues again

--- c/ch03/top_secret/secret_messages.c
+++ c/ch03/top_secret/secret_messages.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-	char word[10];
-	int i = 0;
-	while (scanf("%9s", word) == 1) {
-		i = i + 1;
-		if (i % 2)
-			fprintf(stdout, "%s\n", word);
-		else
-			fprintf(stderr, "%s\n", word);
-	}
-	return 0;
+    char word[10];
+    int i = 0;
+    while (scanf("%9s", word) == 1) {
+        i = i + 1;
+        if (i % 2)
+            fprintf(stdout, "%s\n", word);
+        else
+            fprintf(stderr, "%s\n", word);
+    }
+    return 0;
 }
