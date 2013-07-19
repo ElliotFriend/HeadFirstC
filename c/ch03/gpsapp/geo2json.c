5e36842249f2e5fd549f132d80e44a4aacb920c2
Elliot Voris <elliotfriend@gmail.com>
2013-07-18 22:23:23 -0500

    fixed some spacing issues

--- c/ch03/gpsapp/geo2json.c
+++ c/ch03/gpsapp/geo2json.c
@@ -2,33 +2,33 @@
 
 int main()
 {
-	float latitude;
-	float longitude;
-	char info[80];
-	int error = 0;
-	int started = 0;
-	
-	puts("data=[");
-	while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3) {
-		if (started)
-			printf(",\n");
-		else
-			started = 1;
-		
-		if ((latitude < -90.0) || (latitude > 90.0)) {
-			fprintf(stderr, "Invalid latitude: %f\n", latitude);
-			error = 2;
-		}
-		if ((longitude < -180.0) || (longitude > 180.0)) {
-			fprintf(stderr, "Invalid longitude: %f\n", longitude);
-			error = 2;
-		}
-					
-		printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
-	}
-	puts("\n]");
-	if (error == 2)
-		return 2;
-	else
-		return 0;
+    float latitude;
+    float longitude;
+    char info[80];
+    int error = 0;
+    int started = 0;
+    
+    puts("data=[");
+    while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3) {
+        if (started)
+            printf(",\n");
+        else
+            started = 1;
+
+        if ((latitude < -90.0) || (latitude > 90.0)) {
+            fprintf(stderr, "Invalid latitude: %f\n", latitude);
+            error = 2;
+        }
+        if ((longitude < -180.0) || (longitude > 180.0)) {
+            fprintf(stderr, "Invalid longitude: %f\n", longitude);
+            error = 2;
+        }
+        
+        printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
+    }
+    puts("\n]");
+    if (error == 2)
+        return 2;
+    else
+        return 0;
 }
