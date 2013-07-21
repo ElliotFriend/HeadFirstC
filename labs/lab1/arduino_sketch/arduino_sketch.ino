f9a007f5f8b0b2d820682f943b9099b64e654ad7
Elliot Voris <elliotfriend@gmail.com>
2013-07-20 23:10:40 -0500

    Wrote the C code to check for moisture level, and alert appropriately. Compiles and works as expected.

--- labs/lab1/arduino_sketch/arduino_sketch.ino
+++ labs/lab1/arduino_sketch/arduino_sketch.ino
@@ -1,20 +1,64 @@
-void setup()
-
-{
+/*
+  C Lab 1 (Not to be confused with Sealab 2021)
+  The purpose of this exercise is to build an alert to remind
+  somebody that their plant needs watering.
   
-  /* This is called when the program starts. It
-  basically sets up the board. Put any initialization
-  code here. */
+  There are two nails that will be used as (analog) sensors
+  to read the moisture level in the soil. When that level drops
+  below a certain threshold (800, for starters), the LED should
+  light.
+*/
+
+// Pin 13 is the LED that will be our signal when moisture levels
+// in the soil drops too low.
+int led = 13;
+// Pin A0 is the analog pin connected to one of our nail sensors
+int sensorPin = A0;
+int sensorValue = 0; // somewhere to store the value
+int lastValue = 0; // a variable to compare values
+
+
+void setup() {
+  
+  // This is called when the program starts. It
+  // basically sets up the board. Put any initialization
+  // code here.
+  
+  // initialize our digital LED pin as output
+  pinMode(led, OUTPUT);
+  
+  // initialize serial communication at 9600 baud
+  Serial.begin(9600);
   
 }
 
-void loop()
-
-{
+void loop() {
   
-  /* This is where your main code goes. This function
-  loops over and over, and allows you to respond to
-  input from your sensors. It only stops running when
-  the board is switched off */
+  // This is where your main code goes. This function
+  // loops over and over, and allows you to respond to
+  // input from your sensors. It only stops running when
+  // the board is switched off
   
+  // read the value from our nail sensor
+  sensorValue = analogRead(sensorPin);
+  
+  // if that value is below 800, light up the LED and write
+  // a message
+  if (sensorValue < 800) {
+    Serial.println("Feed me!");
+    digitalWrite(led, HIGH);
+    // store the sensor value for later comparison
+    lastValue = sensorValue;
+  }
+  // otherwise,
+  else {
+    // if this is the first time that the sensor has read
+    // a normal reading, write a message
+    if (lastValue < 800) {
+      Serial.println("Thank you, Seymour!");
+    }
+    // turn off the LED
+    digitalWrite(led, LOW);
+    lastValue = sensorValue;
+  }
 }
