cbb8ab702d7316b7a3284a5038d85aaedd5abd79
Elliot Voris <elliotfriend@gmail.com>
2013-07-22 00:29:49 -0500

    created and used the find.c program

--- ch07/find.c
+++ ch07/find.c
@@ -4,20 +4,33 @@
 int NUM_ADS = 7;
 char *ADS[] = {
     "William: SBM GSOH likes sports, TV, dining",
-    "Matt: SWM NS like sart, movies, theater",
+    "Matt: SWM NS likes art, movies, theater",
     "Luis: SLM ND likes books, theater, art",
-    "Mike: DWM DS like trucks, sports, and bieber",
+    "Mike: DWM DS likes trucks, sports, and bieber",
     "Peter: SAM likes chess, working out, and art",
     "Josh: SJM likes sports, movies, and theater",
-    "Jed: DBM like theater, books, and dining"
+    "Jed: DBM likes theater, books, and dining"
 };
 
-void find() {
+int sports_no_bieber(char *s) {
+    return strstr(s, "sports") && !strstr(s, "bieber");
+}
+int sports_or_workout(char *s) {
+    return strstr(s, "sports") || strstr(s, "working out");
+}
+int ns_theater(char *s) {
+    return strstr(s, "NS") && strstr(s, "theater");
+}
+int arts_theater_or_dining(char *s) {
+    return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
+void find(int(*match)(char*)) {
     int i;
     puts("Search Results:");
     puts("------------------------------------------------");
     for (i = 0; i < NUM_ADS; i++) {
-        if (strstr(ADS[i], "sports") && !strstr(ADS[i], "bieber")) {
+        if (match(ADS[i])) {
             printf("%s\n", ADS[i]);
         }
     }
@@ -25,5 +38,9 @@ void find() {
 }
 
 int main() {
-    find();
+    find(sports_no_bieber);
+    find(sports_or_workout);
+    find(ns_theater);
+    find(arts_theater_or_dining);
+    return 0;
 }
